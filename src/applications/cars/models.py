dae9732a5a0b982a2d31b2a957f7e8103ed15ce9
Franco <franco.reinaldi@hotmail.com>
2023-08-01 15:55:44 -0300

    se crean modelos manufacturer y car

--- src/applications/cars/models.py
+++ src/applications/cars/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Manufacturer(models.Model):
+    name = models.CharField()
+
+class Car(models.Model):
+    manufacturer = models.ForeignKey(Manufacturer, on_delete=models.CASCADE)
+    model = models.CharField()
+    year = models.IntegerField(blank=True,null=True)
