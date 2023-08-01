0f490aa435113f559ac53fcc539400dd4699a9ae
Franco <franco.reinaldi@hotmail.com>
2023-08-01 15:57:32 -0300

    se agregan migraciones de manufacturer y car

--- src/applications/cars/models.py
+++ src/applications/cars/models.py
@@ -1,9 +1,9 @@
 from django.db import models
 
 class Manufacturer(models.Model):
-    name = models.CharField()
+    name = models.CharField(max_length=100)
 
 class Car(models.Model):
     manufacturer = models.ForeignKey(Manufacturer, on_delete=models.CASCADE)
-    model = models.CharField()
+    model = models.CharField(max_length=100)
     year = models.IntegerField(blank=True,null=True)
