f7fb05e3409b3b6a87e5f6edceaff620ea2e2bb7
ManuelPerea13 <manuel.perea@corbisstudio.com>
2023-08-09 03:03:15 -0300

    simplify model user

--- src/applications/users/models.py
+++ src/applications/users/models.py
@@ -3,35 +3,37 @@ from django.contrib.auth.models import AbstractUser, BaseUserManager
 
 
 class UserManager(BaseUserManager):
-    def create_user(self, username, email, phone, address, password=None, **extra_fields):
+    def create_user(self, email, username, password=None):
         if not email:
             raise ValueError('The Email field must be set')
 
+        user = self.model(username=username, email=email)
         email = self.normalize_email(email)
-        user = self.model(username=username, email=email, phone=phone, address=address, **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
         return user
 
-    def create_superuser(self, username, email, phone, address, password=None, **extra_fields):
-        extra_fields.setdefault('is_staff', True)
-        extra_fields.setdefault('is_superuser', True)
-
-        if extra_fields.get('is_staff') is not True:
-            raise ValueError('Superuser must have is_staff=True.')
-        if extra_fields.get('is_superuser') is not True:
-            raise ValueError('Superuser must have is_superuser=True.')
-
-        return self.create_user(username, email, phone, address, password, **extra_fields)
+    def create_superuser(self, email, username, password=None):
+        user = self.create_user(email,
+                                username,
+                                password=password)
+        user.is_admin = True
+        user.is_staff = True
+        user.save(using=self._db)
+        return user
 
 
 class User(AbstractUser):
-    username = models.CharField(max_length=255, unique=True)
     email = models.EmailField(unique=True)
+    username = models.CharField(max_length=255, unique=True)
     phone = models.CharField(max_length=20)
     address = models.TextField()
 
-    USERNAME_FIELD = 'username'
+    EMAIL_FIELD = "email"
+    USERNAME_FIELD = "username"
+    REQUIRED_FIELDS = ["email"]
+
+    objects = UserManager()
 
     def __str__(self):
-        return self.username
+        return self.email
