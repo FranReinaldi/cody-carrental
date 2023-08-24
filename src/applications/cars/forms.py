78f804d2fd6b0735cc1451bc11a52d371277def9
Franco <franco.reinaldi@hotmail.com>
2023-08-24 10:57:49 -0300

    add car price modification

--- src/applications/cars/forms.py
+++ src/applications/cars/forms.py
@@ -10,13 +10,14 @@ class ManufacturerForm(forms.ModelForm):
 class CarForm(forms.ModelForm):
     class Meta:
         model = Car
-        fields = ['manufacturer', 'model','year']
+        fields = ['manufacturer', 'model','year','daily_price']
 
     def __init__(self, *args, **kwargs):
         super(CarForm, self).__init__(*args, **kwargs)
         self.fields['manufacturer'].widget = forms.Select(choices=self.fields['manufacturer'].choices, attrs={'class': 'form-control'})
         self.fields['model'].widget = forms.TextInput(attrs={'class': 'form-control'})
         self.fields['year'].widget = forms.TextInput(attrs={'class': 'form-control'})
+        self.fields['daily_price'].widget = forms.TextInput(attrs={'class': 'form-control'})
 
 class RentalForm(forms.ModelForm):
     class Meta:
