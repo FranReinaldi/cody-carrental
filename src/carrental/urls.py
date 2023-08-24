72fa0a77d059e860431dc831724f7dc061211efd
Jose Francisco Cortes <franjo.cortes@gmail.com>
2023-08-24 11:19:30 -0300

    se acomoda una pagina home

--- src/carrental/urls.py
+++ src/carrental/urls.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
 
+from applications.core.views import welcome
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('cars/',include('applications.cars.urls')),
     path('users/', include('applications.users.urls')),
+    path('', welcome, name='welcome'),
 ]
